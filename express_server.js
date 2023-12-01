b08ed511ee0a985732887f57ff1fd6a094093352
Alysha Huot <alyshajhuot@gmail.com>
2023-11-30 21:36:06 -0700

    install cookie-session and refactor everything to use cookie-session instead of cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 const bcrypt = require("bcryptjs");
 
 app.set("view engine", "ejs"); // set ejs as the template engine
@@ -67,19 +68,28 @@ const urlsForUser = function(id) {
 // parse URL encoded data
 app.use(express.urlencoded({ extended: true}));
 
-app.use(cookieParser());
+// app.use(cookieParser());
+
+const key1 = generateRandomString();
+const key2 = generateRandomString();
+
+app.use(cookieSession({
+  name: "session",
+  keys: [key1, key2],
+  maxAge: 24 * 60 * 60 * 1000
+}));
 
 /////////////ENDPOINTS/////////////////
 
 
 // when client requests "/urls", render the "urls_index" template with urlDatabase
 app.get("/urls", (req, res) => {
-  if (!req.cookies.user_id) {
+  if (!req.session.user_id) {
     res.send('You need to be logged in to see this content. <a href="/login">Login</a>');
     return;
   }
 
-  const id = req.cookies.user_id;
+  const id = req.session.user_id;
   const userURLS = urlsForUser(id);
 
   const templateVars = { 
@@ -93,7 +103,7 @@ app.get("/urls", (req, res) => {
 // create endpoint that renders a webpage that allows users to submit their longURL to get a short one
 app.get("/urls/new", (req, res) => {
   const templateVars = { users, req };
-  if (!req.cookies.user_id) {   // if user isn't logged in, redirect them to login page
+  if (!req.session.user_id) {   // if user isn't logged in, redirect them to login page
     res.redirect("/login");
     return;
   }
@@ -103,14 +113,14 @@ app.get("/urls/new", (req, res) => {
 // endpoint to create new shortURL
 app.post("/urls", (req, res) => {
   
-  if (!req.cookies.user_id) {   // check if user is logged in - if not, send them a message telling them to log in
+  if (!req.session.user_id) {   // check if user is logged in - if not, send them a message telling them to log in
     res.send('You need to log in to access this service. <a href="/login">Login</a>');
     return;
   }
 
   const shortURL = generateRandomString();  // create short URL to give to client
 
-  urlDatabase[shortURL] = {longURL: req.body.longURL, userID: req.cookies.user_id};  // save shortURL, longURL, and userID to database
+  urlDatabase[shortURL] = {longURL: req.body.longURL, userID: req.session.user_id};  // save shortURL, longURL, and userID to database
   
   res.redirect(`/urls/${shortURL}`);  // redirect client to "/urls/:id", with their shortURL as the URL parameter
 });
@@ -118,7 +128,7 @@ app.post("/urls", (req, res) => {
 // endpoint to delete URL from database
 app.post("/urls/:id/delete", (req, res) => {
 
-  const id = req.cookies.user_id;
+  const id = req.session.user_id;
   const shortURL = req.params.id;
 
   if (!id) { // ensure user is logged in before they delete a URL
@@ -142,9 +152,10 @@ app.post("/urls/:id/delete", (req, res) => {
 
 // endpoint to update longURL
 app.post("/urls/:id/update", (req, res) => {
-  const id = req.cookies.user_id;
+  const id = req.session.user_id;
   const shortUrl = req.params.id;
   const longUrl = req.body.longURL;
+  console.log("req.body:", req.body);
 
   if (urlDatabase[shortUrl].userID !== id) { // check if url they want to update belongs to user
     res.send('You cannot edit the urls of other users. Log in to your own account here: <a href="/login">Log In</a>');
@@ -155,10 +166,16 @@ app.post("/urls/:id/update", (req, res) => {
   res.redirect("/urls");
 });
 
+// endpoint that directs users to the update page when they click the edit button
+app.post("/urls/:id/edit", (req, res) => {
+  const shortURL = req.params.id;
+  res.redirect(`/urls/${shortURL}`);
+});
+
 // render login page
 app.get("/login", (req, res) => {
   const templateVars = { users, req };
-  if (req.cookies.user_id) {
+  if (req.session.user_id) {
     res.redirect("/urls");
     return;
   }
@@ -184,20 +201,20 @@ app.post("/login", (req, res) => {
   };
 
   const id = userFromEmail.id;
-  res.cookie("user_id", users[id].id); // creates a cookie with user object
+  req.session.user_id = users[id].id; // used to be res.cookie("user_id", users[id].id) // creates a cookie with user object
   res.redirect("/urls");
 });
 
 // logout form endpoint
 app.post("/urls/logout", (req, res) => {
-  res.clearCookie("user_id");
+  req.session = null; // used to be res.clearCookie("user_id") -> clears cookie(s)
   res.redirect("/login");
 });
 
 // render registration page
 app.get("/register", (req, res) => {
   const templateVars = { users, req };
-  if (req.cookies.user_id) {
+  if (req.session.user_id) {
     res.redirect("/urls");
     return;
   }
@@ -224,14 +241,14 @@ app.post("/register", (req, res) => {
 
   users[id] = { id, email, hashedPassword }; // add new user to users object
   console.log("user database:", users);
-  res.cookie("user_id", users[id].id); // create cookie that allows user to remain logged in
+  req.session.user_id = users[id].id; // create cookie that allows user to remain logged in
   res.redirect("/urls");
 });
 
 // create endpoint that takes in URL parameters (urls_show)
 app.get("/urls/:id", (req, res) => {
 
-  if (!req.cookies.user_id) {  // if user isn't logged in, send them message telling them to log in
+  if (!req.session.user_id) {  // if user isn't logged in, send them message telling them to log in
     res.send('You must be logged in to access this page. <a href="/login">Login</a>');
     return;
   }
@@ -243,7 +260,7 @@ app.get("/urls/:id", (req, res) => {
     return;
   }
 
-  const userURLS = urlsForUser(req.cookies.user_id);
+  const userURLS = urlsForUser(req.session.user_id);
   if (!userURLS[urlParameters]) {
     res.send('This shortURL belongs to someone else! Create your own shortURL here: <a href="/urls/new">Create ShortURL</a>');
     return;
